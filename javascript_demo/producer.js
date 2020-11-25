86100b100ea2663f4cee4ade7b73a3656e2ed9de
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-11-24 20:53:51 -0800

    add producers
    -batch message sending
    -one-by-one on an interval

---
+++ javascript_demo/producer.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Chance = require('chance');
+const { kafka } = require('./kafka.js');
+
+const chance = new Chance();
+const producer = kafka.producer();
+
+let count = 50;
+let topic;
+
+const animalMessage = async () => {
+  const animal = chance.animal();
+  const message = createMessage(animal);
+  console.log(animal);
+  await sendMessage(producer, topic, message);
+};
+
+const sendMessage = async (producer, topic, message) => {
+  try {
+    await producer.send({ topic, messages: [message] });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const sendBatchMessages = async (producer, topic, messages) => {
+  try {
+    await producer.send({ topic, messages });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createMessage = (value, key) => ({
+  key: key ? String(key) : String(value),
+  value: String(value),
+});
+
+const runProducer = async (args) => {
+  console.log({args});
+  if (args.length >= 2) {
+    count = parseInt(args[1]);
+  }
+  if (args.length >= 1) {
+    topic = args[0];
+
+    // Sending multiple messages at one time
+    // let messages = new Array(count).fill().map((_value, i) => createMessage(i));
+    // await producer.connect();
+    // await sendBatchMessages(producer, topic, messages);
+
+    // Sending one message every 1000 ms
+    await producer.connect();
+    setInterval(animalMessage, 1000);
+  }
+};
+
+module.exports = { runProducer };
+
+const args = process.argv.slice(2);
+runProducer(args).catch(console.error);
