4d7afb058cb567995e5714afde5fff67df85f0a6
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-11-25 18:54:31 -0800

    add kaffe config

--- elixir_demo/config/config.exs
+++ elixir_demo/config/config.exs
@@ -8,3 +8,15 @@ import Config
   # ],
   # consumer_group: "elixir-group",
   # client_id: "kafka_ex"
+
+config :kaffe,
+  producer: [
+    endpoints: [localhost: 9092, localhost: 9093, localhost: 9094],
+    topics: ["cities"]
+  ],
+  consumer: [
+    endpoints: [localhost: 9092, localhost: 9093, localhost: 9094],
+    topics: ["cities"],
+    consumer_group: "elixir-consumer-group",
+    message_handler: ElixirDemo.Consumer
+  ]
